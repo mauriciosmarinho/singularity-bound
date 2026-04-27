bab7e7dca43adc11ebfe64ba6ed86f8273b18b83
Mauricio Marinho <mauricio.marinho@iesb.edu.br>
2026-04-27 18:24:20 -0300

    Week 4: Colonize sector with fleet implemented on Web interface

--- interface/urls.py
+++ interface/urls.py
@@ -1,7 +1,24 @@
 from django.urls import path
-from .views import dashboard, next_turn_web
+from .views import (
+    dashboard, 
+    next_turn_web,
+    explore_sector_web,
+    colonize_web,
+    fleet_web,
+    research_web,
+    spy_web,
+    negotiate_web,
+    attack_web, 
+)
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
     path("next-turn/", next_turn_web, name="next_turn_web"),
+    path("explore-sector/", explore_sector_web, name="explore_sector_web"),
+    path("colonize/", colonize_web, name="colonize_web"),
+    path("fleet/", fleet_web, name="fleet_web"),
+    path("research/", research_web, name="research_web"),
+    path("spy/<str:faction_name>/", spy_web, name="spy_web"),
+    path("negotiate/", negotiate_web, name="negotiate_web"),
+    path("attack/", attack_web, name="attack_web"),
 ]
--- interface/views.py
+++ interface/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-
+from django.core.management import call_command
+from io import StringIO
+from django.contrib import messages
 from players.models import Commander
 from turns.models import Turn
-from core.models import Faction, Sector
+from core.models import Faction, Sector, Fleet
 from core.services.turn_engine import TurnEngine
 
 
@@ -12,20 +14,86 @@ def dashboard(request):
     turn = Turn.objects.order_by("-number").first()
     factions = Faction.objects.all()
     sectors = Sector.objects.all()
+    fleets = Fleet.objects.all()
 
     context = {
         "commander": commander,
         "turn": turn,
         "factions": factions,
         "sectors": sectors,
+        "fleets": fleets,
     }
 
     return render(request, "interface/dashboard.html", context)
 
 
 def next_turn_web(request):
+    if request.method == "POST":
+        out = StringIO()
+        call_command(
+            "next_turn",
+            stdout=out
+        )
+        aura_text = out.getvalue()
+        messages.success(request, aura_text)
+        #TurnEngine().run()
+    return redirect("dashboard")
 
-    engine = TurnEngine()
-    engine.run()
+def explore_sector_web(request):
+    if request.method == "POST":
+        out = StringIO()
+        call_command(
+            "explore_sector",
+            stdout=out
+        )
+        aura_text = out.getvalue()
+        messages.success(request, aura_text)
+    return redirect("dashboard")
 
+def colonize_web(request):
+    if request.method =="POST":
+        out = StringIO()
+        call_command("colonize_sector", 
+            request.POST['fleet_name'], 
+            request.POST['sector_name'],
+            stdout=out
+        )
+        aura_text = out.getvalue()
+        messages.success(request, aura_text)
+    return redirect("dashboard")
+
+def fleet_web(request):
+    if request.method == "POST":
+        call_command("build_fleet")
+    return redirect("dashboard")
+
+def research_web(request):
+    if request.method == "POST":
+        call_command("start_research")
+    return redirect("dashboard")
+
+def spy_web(request, faction_name):
+    if request.method == "POST":
+        out = StringIO()
+
+        call_command(
+            "spy_on",
+            faction_name,
+            stdout=out
+        )
+
+        aura_text = out.getvalue()
+
+        messages.success(request, aura_text)
+
+    return redirect("dashboard")
+
+def negotiate_web(request):
+    if request.method == "POST":
+        call_command("negotiate")
+    return redirect("dashboard")
+
+def attack_web(request):
+    if request.method == "POST":
+        call_command("attack_faction")
     return redirect("dashboard")
